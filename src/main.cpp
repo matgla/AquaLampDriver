cfca320d7b29fe90901a6ddb7a4f66e932fbb62a
Mateusz Stadnik <matgla@live.com>
2017-10-31 00:48:54 +0100

    working

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ void initializeBoardLeds()
 void initializeButtons()
 {
     GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_12;
+    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_4 | GPIO_Pin_5;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
